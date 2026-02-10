eb89a0a102ef7ae9de517667cda09c37ef1e9695
Trent McNair <trent@desktop.boise.tvi910.com>
2026-02-09 18:44:32 -0700

    update

--- gen.sh
+++ gen.sh
@@ -6,10 +6,10 @@ for dir in $(find ./content -type d)
 do
     if [ -f "${dir}/index.html" ]; then
         mkdir -p "./staging/${dir}"
-        cp header1.html "./staging/${dir}/index.html"
-        cat header2.html >> "./staging/${dir}/index.html"
+        cp content/header1.html "./staging/${dir}/index.html"
+        cat content/header2.html >> "./staging/${dir}/index.html"
         cat "${dir}/index.html" >> "./staging/${dir}/index.html"
-        cat footer.html >> "./staging/${dir}/index.html"
+        cat content/footer.html >> "./staging/${dir}/index.html"
         echo "$dir"
     fi
 done
